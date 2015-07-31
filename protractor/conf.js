b7d291316e98c64c5ec15eef2ebc391c4290e0e6
rosaline <rosaline.zeng@gmail.com>
2015-07-31 16:14:00 +0800

    roll back conf.js

--- protractor/conf.js
+++ protractor/conf.js
@@ -4,10 +4,10 @@
 
 // conf.js
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['addCourses.js','addUser.js','courseConvert.js','enrollment.js'],
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['addCourses.js','addUser.js','courseConvert.js','enrollment.js'],
     //specs: ['addCourses.js','courseConvert.js'],
-	//specs: ['courseConvert.js'],
+    //specs: ['courseConvert.js'],
     //specs: ['enrollment.js'],
     //specs: ['addCourses.js'],
 
@@ -20,33 +20,33 @@ exports.config = {
 
     onPrepare: function() {
 
-    	// global.isAngularSite = function (flag) {
-    	// 	browser.ignoreSynchronization = !flag;
-    	// }
+        // global.isAngularSite = function (flag) {
+        //  browser.ignoreSynchronization = !flag;
+        // }
 
 
-    	//Set window size before starting the tests
-		browser.driver.manage().window().setSize(1920, 1200);
-		//browser.driver.manage().window().maxisize();
+        //Set window size before starting the tests
+        browser.driver.manage().window().setSize(1920, 1200);
+        //browser.driver.manage().window().maxisize();
 
-    	var folderName = (new Date()).toString().split(' ').splice(1, 4).join('-');
-    	var mkdirp = require('mkdirp');
-    	var newFolder = "./reports/" + folderName;
-    	require('jasmine-reporters');
+        var folderName = (new Date()).toString().split(' ').splice(1, 4).join('-');
+        var mkdirp = require('mkdirp');
+        var newFolder = "./reports/" + folderName;
+        require('jasmine-reporters');
 
-    	mkdirp(newFolder,function(err) {
-    		if (err) {
-    			console.error(err);
-    		} else {
-    			jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(newFolder, true, true));
-    		}
-    	});
+        mkdirp(newFolder,function(err) {
+            if (err) {
+                console.error(err);
+            } else {
+                jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(newFolder, true, true));
+            }
+        });
     },
 
- 	jasmineNodeOpts: {
-		showColors: true,
-		defaultTimeoutInterval: 120000,
-		isVerbose: true,
-		includeStackTrace: true
-	}
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 120000,
+        isVerbose: true,
+        includeStackTrace: true
+    }
 };
